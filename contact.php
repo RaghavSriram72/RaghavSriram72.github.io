0cabd98001d62fd5dfec9134acacfc238a67623e
Raghav Sriram <73449126+RaghavSriram72@users.noreply.github.com>
2021-01-17 13:29:57 -0500

    Dasdf

--- contact.php
+++ contact.php
@@ -1,55 +1,25 @@
 <?php
-
-$errors = [];
-$errorMessage = '';
-
-$secret = 'your secret key';
-
-if (!empty($_POST)) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $recaptchaResponse = $_POST['g-recaptcha-response'];
-
-    $recaptchaUrl = "https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$recaptchaResponse}";
-    $verify = json_decode(file_get_contents($recaptchaUrl));
-
-    if (!$verify->success) {
-      $errors[] = 'Recaptcha failed';
-    }
-
-    if (empty($name)) {
-        $errors[] = 'Name is empty';
-    }
-
-    if (empty($email)) {
-        $errors[] = 'Email is empty';
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Email is invalid';
-    }
-
-    if (empty($message)) {
-        $errors[] = 'Message is empty';
-    }
-
-
-    if (!empty($errors)) {
-        $allErrors = join('<br/>', $errors);
-        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
-    } else {
-        $toEmail = 'rsriram101@gmail.com';
-        $emailSubject = 'New email from your contant form';
-        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
-
-        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
-        $body = join(PHP_EOL, $bodyParagraphs);
-
-        if (mail($toEmail, $emailSubject, $body, $headers)) {
-            header('Location: thank-you.html');
-        } else {
-            $errorMessage = "<p style='color: red;'>Oops, something went wrong. Please try again later</p>";
-        }
-    }
-}
-
+$name = $_POST['firstname'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+ 
+$email_from = "$visitor_email.\n";
+ 
+$email_subject = "$subject\n";
+ 
+$email_body = "Name: $name.\n".
+				"Message: $message\n";
+ 
+$to = "rsriram101@gmail.com";
+ 
+ 
+$headers = "From: $email_from \r\n";
+ 
+$headers .= "Reply-To: $visitor_email \r\n";
+ 
+mail($to,$email_subject,$email_body,$headers);
+ 
+header("Location: index.html");
+ 
 ?>
